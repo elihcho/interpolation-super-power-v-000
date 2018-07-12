e5d9a61c6efcbe39aa1f0517e6dbb201953a598f
Eli Cho <eli.cho@gmail.com>
2018-07-12 06:21:05 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   puts "R: colors[0]"
+  puts "R: colors[1]"
+  puts "R: colors[2]"
+  puts "R: colors[3]"
+  puts "R: colors[4]"
+  puts "R: colors[5]"
+  puts "R: colors[6]"
 end
